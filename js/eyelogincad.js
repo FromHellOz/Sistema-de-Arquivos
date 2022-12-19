c31c06940df5916eec33d2ea19bae9e957a6207f
Hellô <118556148+FromHellOz@users.noreply.github.com>
2022-12-19 10:02:51 -0300

    Delete js directory

--- js/eyelogincad.js
+++
@@ -1,5 +0,0 @@
-let input = document.querySelector('#input input');
-let img = document.querySelector('#input img');
-img.addEventListener('click', function () {
-  input.type = input.type == 'text' ? 'password' : 'text';
-});
