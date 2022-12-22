6b771d149961e2bca6c338e53c5a61a14b50cac8
Hellô <118556148+FromHellOz@users.noreply.github.com>
2022-12-22 13:56:46 -0300

    Add files via upload

---
+++ Testes/app.py
@@ -0,0 +1,5 @@
+import sql_server
+con = sql_server.connect(host = '10.0.0.89',
+                      user = 'sa',
+                      password = 'l3it3@1000',
+                      database = 'prontuario')
